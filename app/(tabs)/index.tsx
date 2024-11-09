6d28e93d0daca046ffd75b33f5eccbf8d4039652
B-chandru <thidrkavikanchandru@gmail.com>
2024-11-09 18:35:01 +0530

    first upload

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,145 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View , FlatList, TouchableOpacity} from "react-native";
 
 export default function HomeScreen() {
+  const [task, setTask] = useState('');
+   const [btntext, setBtntext] = useState(-1);
+  const [tasklist, setaTasklist] = useState([]);
+
+  const handleaddtask =()=> {
+    if(task){
+      if (btntext !== -1) {
+        const updatedtask = [...tasklist];
+        updatedtask[btntext] = task;
+        setaTasklist(updatedtask);
+        setBtntext(-1);
+      } else {
+        setaTasklist([...tasklist, task]);
+      }
+      setTask("");
+    }
+  
+  }
+
+
+     const renderitem =({item, index})=>{
+        return (
+          <View style={styles.list_container}>
+            <Text style={styles.list_h2}>{item}</Text>
+            <View style={styles.inner_list_con}>
+              <TouchableOpacity onPress={() => Edittask(index)}>
+                <Text style={styles.list_edit}>
+                  <MaterialIcons name="edit" size={24} color="green" />
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => Deletetask(index)}>
+                <Text style={styles.list_del}>
+                  <MaterialCommunityIcons
+                    name="delete"
+                    size={24}
+                    color="red"
+                  />
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+  }
+  
+ 
+    const Edittask = (item) => {
+      const tasktoedit = tasklist[item];
+      setTask(tasktoedit);
+      setBtntext(item);
+    };
+
+      const Deletetask = (item) => {
+        const updatedtask= [...tasklist];
+        updatedtask.splice(item,1);
+        setaTasklist(updatedtask);
+      };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <ScrollView style={styles.titlecon}>
+      <Text style={styles.title_h1}>Be productive</Text>
+      <View>
+      {/* Textinput */}
+        <TextInput
+          placeholder="Enter task"
+          onChangeText={(e) => setTask(e)}
+          style={styles.txtinput}
+          value={task}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      {/*  button */}
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={handleaddtask}
+        >
+          <Text style={styles.enter_btn}>{btntext !== -1 ?" Update task": "Add Task"}</Text>
+        </TouchableOpacity>
+
+        {/* lising */}
+        <FlatList data={tasklist} renderItem={renderitem} keyExtractor={(item, index)=> index.toString()}/>
+
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  titlecon: {
+    flex: 1,
+    padding: 40,
+    marginTop: 20,
+    backgroundColor: "white",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title_h1: {
+    fontSize: 30,
+    textAlign: "center",
+    textTransform: "capitalize",
+    paddingBottom: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  txtinput: {
+    marginTop: 20,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderBlockColor: "#37474f",
+    padding: 10,
+    borderRadius: 4,
   },
+  btn: {
+    backgroundColor: "#f44336",
+    padding: 10,
+    borderRadius: 4,
+  },
+  enter_btn: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+  list_container:{
+  padding:10,
+  marginTop:10,
+  marginBottom:10,
+  display:'flex',
+  flexDirection:'row',
+  justifyContent:'space-between'
+  },
+  list_h2:{
+    fontWeight:'bold',
+  },
+  inner_list_con:{
+    display:'flex',
+    flexDirection:'row',
+  },
+  list_edit:{
+    color:'#4caf50'
+  },
+  list_del:{
+    color:'red'
+  }
+  
 });
